149360e3766ffb0a14bf70daaba0f092f3a44d25
notBnumber <15625738634@163.com>
2019-06-29 18:19:58 +0800

    2019年6月29日18:19:55

--- pages/chooseCity/chooseCity.js
+++ pages/chooseCity/chooseCity.js
@@ -1,4 +1,6 @@
 // pages/chooseCity/chooseCity.js
+const util = require("../../utils/util.js");
+
 Page({
 
   /**
@@ -6,38 +8,38 @@ Page({
    */
   data: {
     cityList:[
-      {title: 'A',valList:['阿萨德','奥司法','按格式','阿飞']},
-      {title: 'B',valList:['巴斯夫','八十分','爸爸']},
-      {title: 'C',valList:['茶水费','茶树网','从头','查收']},
-      {title: 'D',valList:['大帅哥','带我去','达芬奇','订单']},
-      {title: 'E',valList:['二手房','而']},
-      {title: 'F',valList:['发顺丰','发生','发送']},
-      {title: 'G',valList:['刚','高级','挂失','个人意见']},
-      {title: 'H',valList:['划算日期','哈飒飒','哈哈哈哈','荷叶']},
-      {title: 'I',valList:['压缩','亚索','牙刷','因为']},
-      {title: 'J',valList:['假设法','举案说法','假的离开','几千万人']},
-      {title: 'K',valList:['咖啡','卡死','考古家','看功能']},
-      {title: 'L',valList:['拉司法所','拉加适量','拉升佛','来看看']},
-      {title: 'N',valList:['那份','那个离开','那个','那天']},
-      {title: 'M',valList:['麻烦','马上','马赛克','马飞']},
-      {title: 'O',valList:['omg','哦','噢']},
-      {title: 'P',valList:['骗了','漂亮','屁','骗子']},
-      {title: 'Q',valList:['千万人防','清水房','青岛市','请我']},
-      {title: 'R',valList:['染色费','燃烧','然后','日月凌空']},
-      {title: 'S',valList:['沙发','是按格式','是个','是好']},
-      {title: 'T',valList:['他是否',' 通过','泰拉科技和','他说']},
-      {title: 'U',valList:['ui']},
-      {title: 'V',valList:['vvvvv','vv']},
-      {title: 'W',valList:['挖是否','忘','问题挖','我还根深蒂固']},
-      {title: 'X',valList:['徐秩序','小程序','下噶','小样']},
-      {title: 'Y',valList:['压他','亚沙','呀','亚商所']},
-      {title: 'Z',valList:['咋收费','早','造','炸天']},
+      {keyword: 'A',valList:['阿萨德','奥司法','按格式','阿飞']},
+      {keyword: 'B',valList:['巴斯夫','八十分','爸爸']},
+      {keyword: 'C',valList:['茶水费','茶树网','从头','查收']},
+      {keyword: 'D',valList:['大帅哥','带我去','达芬奇','订单']},
+      {keyword: 'E',valList:['二手房','而']},
+      {keyword: 'F',valList:['发顺丰','发生','发送']},
+      {keyword: 'G',valList:['刚','高级','挂失','个人意见']},
+      {keyword: 'H',valList:['划算日期','哈飒飒','哈哈哈哈','荷叶']},
+      {keyword: 'I',valList:['压缩','亚索','牙刷','因为']},
+      {keyword: 'J',valList:['假设法','举案说法','假的离开','几千万人']},
+      {keyword: 'K',valList:['咖啡','卡死','考古家','看功能']},
+      {keyword: 'L',valList:['拉司法所','拉加适量','拉升佛','来看看']},
+      {keyword: 'N',valList:['那份','那个离开','那个','那天']},
+      {keyword: 'M',valList:['麻烦','马上','马赛克','马飞']},
+      {keyword: 'O',valList:['omg','哦','噢']},
+      {keyword: 'P',valList:['骗了','漂亮','屁','骗子']},
+      {keyword: 'Q',valList:['千万人防','清水房','青岛市','请我']},
+      {keyword: 'R',valList:['染色费','燃烧','然后','日月凌空']},
+      {keyword: 'S',valList:['沙发','是按格式','是个','是好']},
+      {keyword: 'T',valList:['他是否',' 通过','泰拉科技和','他说']},
+      {keyword: 'U',valList:['ui']},
+      {keyword: 'V',valList:['vvvvv','vv']},
+      {keyword: 'W',valList:['挖是否','忘','问题挖','我还根深蒂固']},
+      {keyword: 'X',valList:['徐秩序','小程序','下噶','小样']},
+      {keyword: 'Y',valList:['压他','亚沙','呀','亚商所']},
+      {keyword: 'Z',valList:['咋收费','早','造','炸天']},
     ],
     position: 'A'
   },
 
   touchCk(e){
-    // console.log(e);
+    console.log(e.target.dataset.id);
     this.setData({
       position: e.target.dataset.id
     })
@@ -60,7 +62,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    util._get('newhome/getHomeArea').then(res=>{
+      if(res.code == 1) {
+        for(let item of res.data) {
+          item.citys = item.cityName.split(',')
+          item.cityIds = item.cityId.split('.')
+        }
+        this.setData({
+          cityList:res.data
+        })
+      }
+    })
   },
 
   /**
--- pages/my/changePhone/index.js
+++ pages/my/changePhone/index.js
@@ -1,78 +1,136 @@
 // pages/my/changePhone/index.js
-Page({
+const util = require("../../../utils/util.js");
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    phone: "",
+    code: "",
+    msg: "发送验证码",
+    time: null,
+    num: 60,
+    phones: ""
   },
+  choosePhone(e) {
+    this.setData({
+      phone: e.detail.value
+    });
+  },
+  chooseCode(e) {
+    this.setData({
+      code: e.detail.value
+    });
+  },
+  choosePhones(e) {
+    this.setData({
+      phones: e.detail.value
+    });
+  },
+  submit() {
+    let params = {
+      sessionId: wx.getStorageSync("sessionId"),
+      code: this.data.code,
+      newMobile: this.data.phones
+    };
+    util._post("account/modifyMobile", params).then(res => {
+      if (res.code == 1) {
+        wx.showToast({
+          title: "修改成功",
+          icon: "success",
+          duration: 1500
+        });
+        setTimeout(() => {
+          wx.navigateBack({
+            delta: 1
+          });
+        }, 1500);
+      }
+    });
+  },
+  send() {
+    // 获取验证码
 
-  submit(){
-    wx.showToast({
-      title: '修改成功',
-      icon: 'success',
-      duration: 1500
-    })
-    setTimeout(()=>{
-      wx.navigateBack({
-        delta: 1
-      })
-    },1500)
+    if (this.data.num == 60) {
+      let params = {
+        mobile: this.data.phone,
+        type: 2
+      };
+      let that = this;
+      util
+        ._get("account/code", params)
+        .then(res => {
+          console.log(res);
+          if (res.code == 1) {
+            let timer = setInterval(function name(params) {
+              if (that.data.num < 1) {
+                that.setData({
+                  msg: "发送验证码",
+                  num: 60
+                });
+              } else {
+                console.log(99999999999999999999999999);
+
+                that.data.num--;
+
+                that.setData({
+                  msg: that.data.num + "s"
+                });
+              }
+            }, 1000);
+            that.setData({
+              time: timer
+            });
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    } else {
+      wx.showToast({
+        title: "请勿连续点击",
+        icon: "none"
+      });
+    }
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
